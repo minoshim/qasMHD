3226a2ac2c0bea5fbe87d16b4e905f2494d2783d
Minoshima Takashi <minoshim@jamstec.go.jp>
2023-05-15 10:37:40 +0900

    minor change

--- 2D/SERIAL/RTI/boundary.c
+++ 2D/SERIAL/RTI/boundary.c
@@ -15,18 +15,20 @@ void boundary(double *p[], int nm, int nx, int ny, int xoff, int yoff,
     int j,ss,sb;
     double fac=(2.0-gamma)/(gamma-1.0);
     double *ro=p[0],*en=p[7];
-    for (j=0;j<yoff;j++){
-      for (i=0;i<nx;i++){
-	ss=nx*j+i;
-	sb=nx*yoff+i;
-	en[ss]=en[sb]-fac*ro[sb]*(phi_g[ss]-phi_g[sb]);
+    {
+      for (j=0;j<yoff;j++){
+	for (i=0;i<nx;i++){
+	  ss=nx*j+i;
+	  sb=nx*yoff+i;
+	  en[ss]=en[sb]-fac*ro[sb]*(phi_g[ss]-phi_g[sb]);
+	}
       }
-    }
-    for (j=ny-yoff;j<ny;j++){
-      for (i=0;i<nx;i++){
-	ss=nx*j+i;
-	sb=nx*(ny-yoff-1)+i;
-	en[ss]=en[sb]-fac*ro[sb]*(phi_g[ss]-phi_g[sb]);
+      for (j=ny-yoff;j<ny;j++){
+	for (i=0;i<nx;i++){
+	  ss=nx*j+i;
+	  sb=nx*(ny-yoff-1)+i;
+	  en[ss]=en[sb]-fac*ro[sb]*(phi_g[ss]-phi_g[sb]);
+	}
       }
     }
   }  
