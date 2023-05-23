2defd41b6141c2935cf0877617b66c756a699c27
Minoshima Takashi <minoshim@jamstec.go.jp>
2023-05-23 14:57:09 +0900

    update plt2d.py

--- 2D/MPI/python/plt2d.py
+++ 2D/MPI/python/plt2d.py
@@ -1,23 +1,61 @@
 import matplotlib.pyplot as plt
 
-def image(x,y,val,save=0,title="",cmap="jet",xlabel="x",ylabel="y",figsize=(6.4,4.8),filename="result.eps",equal=0,show=0,aspect="auto"):
+def image(x,y,val,
+          figsize=(6.4,4.8),
+          aspect="auto",
+          cmap="jet",
+          title="",
+          xlabel="",
+          ylabel="",
+          vmin=None,
+          vmax=None,
+          xmin=None,
+          xmax=None,
+          ymin=None,
+          ymax=None,
+          equal=0,
+          show=0,
+          save=0,
+          colorbar=1,
+          filename="result.eps"):
+    
     fig=plt.figure(figsize=figsize)
 
     if (equal != 0):
         aspect="equal"
     plt.axes().set_aspect(aspect)
 
+    if (vmin == None):
+        vmin=val.min()
+    if (vmax == None):
+        vmax=val.max()
+        
     # plt.pcolormesh(x,y,val,cmap=cmap,shading='auto')
-    plt.imshow(val,cmap=cmap,
-               extent=[x.min(),x.max(),y.min(),y.max()],
-               interpolation='nearest',origin='lower',aspect=aspect) # Fast
+    axesimage=plt.imshow(val,cmap=cmap,
+                         vmin=vmin,vmax=vmax,
+                         extent=[x.min(),x.max(),y.min(),y.max()],
+                         interpolation='nearest',origin='lower',aspect=aspect) # Fast
 
+    if (xmin == None):
+        xmin=x.min()
+    if (xmax == None):
+        xmax=x.max()
+    if (ymin == None):
+        ymin=y.min()
+    if (ymax == None):
+        ymax=y.max()
+    plt.xlim((xmin,xmax))
+    plt.ylim((ymin,ymax))
+    
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
-    plt.colorbar()
+
+    if (colorbar):
+        plt.colorbar()
     if (save):
         fig.savefig(filename)
     if (show != 0):
         plt.show(block=False) # console non-blocked
-    return fig
+
+    return fig, axesimage
