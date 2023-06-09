7bf128e547ff912bac6840e0373a6231eebccb1a
Minoshima Takashi <minoshim@jamstec.go.jp>
2023-06-09 23:10:02 +0900

    add 3D loop image

--- 3D/MPI/loop/mymacros.hpp
+++ 3D/MPI/loop/mymacros.hpp
@@ -13,7 +13,7 @@
 
 // Select solvers.
 #define RMN (2)		 /* Riemann solver (0=Roe, 1=HLLD, 2=LHLLD, 3=MLAU) */
-#define ODR (2)		 /* Spatial order (1,2,3,4). Never set >4 */
+#define ODR (4)		 /* Spatial order (1,2,3,4). Never set >4 */
 #define R_K (3)		 /* Temporal order (1,2,3). Never set >3 */
 #define CTW (1)		 /* Flag for CT 2D upwind weighting (Minoshima+19, ApJS,242,14) */
 
